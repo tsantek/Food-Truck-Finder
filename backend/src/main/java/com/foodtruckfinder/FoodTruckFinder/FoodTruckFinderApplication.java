4c6f8697fa7e0d476ae6674e0e2997a84d9520e1
tsantek <santek.vg@gmail.com>
2019-08-26 09:23:55 -0700

    init backend

---
+++ backend/src/main/java/com/foodtruckfinder/FoodTruckFinder/FoodTruckFinderApplication.java
@@ -0,0 +1,13 @@
+package com.foodtruckfinder.FoodTruckFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTruckFinderApplication.class, args);
+	}
+
+}
