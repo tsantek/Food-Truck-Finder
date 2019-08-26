f156db1dd443c5b6dddb1d76ea056efb27495a09
tsantek <santek.vg@gmail.com>
2019-08-26 11:17:52 -0700

    foodtruck

--- backend/src/main/java/com/foodtruckfinder/FoodTruckFinder/FoodTruck/FoodTruckController.java
+++ backend/src/main/java/com/foodtruckfinder/FoodTruckFinder/FoodTruck/FoodTruckController.java
@@ -1,4 +1,26 @@
 package com.foodtruckfinder.FoodTruckFinder.FoodTruck;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/foodtruck")
 public class FoodTruckController {
+
+    @Autowired
+    public  final FoodTruckRepository foodTruckRepository;
+
+
+    public FoodTruckController(FoodTruckRepository foodTruckRepository) {
+        this.foodTruckRepository = foodTruckRepository;
+    }
+
+    @GetMapping
+    public Iterable<FoodTruck> list(){
+        return foodTruckRepository.findAll();
+    }
 }
