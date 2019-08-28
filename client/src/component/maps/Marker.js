e9be80e4d806a454d321a44638858087b0b0d6c6
tsantek <santek.vg@gmail.com>
2019-08-28 14:02:02 -0700

    markers

--- client/src/component/maps/Marker.js
+++ client/src/component/maps/Marker.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Marker = ({ text }) => <div onClick={e => alert("test")}>{text} </div>;
+const Marker = ({ text }) => {
+  const [state, setState] = useState(false);
+  console.log(state);
+  return (
+    <div
+      className={state ? "pin pin-hover" : "pin"}
+      onMouseEnter={() => setState(true)}
+      onMouseLeave={() => setState(false)}
+    >
+      <div
+        className={
+          state ? "img-mask-marker img-mask-marker-hover " : "img-mask-marker"
+        }
+      ></div>
+      <img
+        alt="img"
+        className="pin-img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTObsbvRGTJUXbhzLkABpz25DUm9L37LV05xAsUpeT8hI4I49Lj"
+      />
+      <div
+        className={state ? "about-marker about-marker-hover" : "about-marker"}
+      >
+        <div className="name-marker">DOG TRUCK</div>
+        <div className="subtitle-marker">Mexican Food</div>
+        <div className="working-hours-marker">10:00am to 2:00 pm</div>
+      </div>
+    </div>
+  );
+};
 
 export default Marker;
