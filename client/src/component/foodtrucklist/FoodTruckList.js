47b0bf31707f52c10ecc3b80b03684190d7c75e6
tsantek <santek.vg@gmail.com>
2019-08-28 11:02:05 -0700

    card

--- client/src/component/foodtrucklist/FoodTruckList.js
+++ client/src/component/foodtrucklist/FoodTruckList.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactStars from "react-stars";
 
 const FoodTruckList = () => {
-  const ratingChanged = newRating => {
-    console.log(newRating);
-  };
-
   return (
     <div className="food-truck-list-container">
       {/* CARD */}
@@ -32,8 +28,89 @@ const FoodTruckList = () => {
             <p className="truck-time-today">From 10:00am to 2:00pm</p>
           </div>
         </div>
-        {/* CARD END */}
       </div>
+      {/* CARD END */}
+
+      {/* CARD */}
+      <div className="card">
+        <div className="img-card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTObsbvRGTJUXbhzLkABpz25DUm9L37LV05xAsUpeT8hI4I49Lj"
+            alt="test"
+            className="img-truck-list"
+          />
+          <div className="card-text">
+            <div className="title-start">
+              <div className="truck-title-card">Dog Truck</div>
+              <ReactStars
+                count={5}
+                size={14}
+                color2={"#ffd700"}
+                value={3}
+                className="stars"
+                edit={false}
+              />
+            </div>
+            <p className="truck-sutitle-card">Mexican Tacos</p>
+            <p className="truck-time-today">From 10:00am to 2:00pm</p>
+          </div>
+        </div>
+      </div>
+      {/* CARD END */}
+
+      {/* CARD */}
+      <div className="card">
+        <div className="img-card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTObsbvRGTJUXbhzLkABpz25DUm9L37LV05xAsUpeT8hI4I49Lj"
+            alt="test"
+            className="img-truck-list"
+          />
+          <div className="card-text">
+            <div className="title-start">
+              <div className="truck-title-card">Dog Truck</div>
+              <ReactStars
+                count={5}
+                size={14}
+                color2={"#ffd700"}
+                value={3}
+                className="stars"
+                edit={false}
+              />
+            </div>
+            <p className="truck-sutitle-card">Mexican Tacos</p>
+            <p className="truck-time-today">From 10:00am to 2:00pm</p>
+          </div>
+        </div>
+      </div>
+      {/* CARD END */}
+
+      {/* CARD */}
+      <div className="card">
+        <div className="img-card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTObsbvRGTJUXbhzLkABpz25DUm9L37LV05xAsUpeT8hI4I49Lj"
+            alt="test"
+            className="img-truck-list"
+          />
+          <div className="card-text">
+            <div className="title-start">
+              <div className="truck-title-card">Dog Truck</div>
+              <ReactStars
+                count={5}
+                size={14}
+                color2={"#ffd700"}
+                value={3}
+                className="stars"
+                edit={false}
+              />
+            </div>
+            <p className="truck-sutitle-card">Mexican Tacos</p>
+            <p className="truck-time-today">From 10:00am to 2:00pm</p>
+          </div>
+        </div>
+      </div>
+      {/* CARD END */}
     </div>
   );
 };
